32d854d5eba24fec728752bfb647e3c37f3ed6ba
Matthew Condie <mattcondie@ldschurch.org>
2014-11-21 17:13:51 -0700

    db basics

--- db/migrations/20141121100318_CreateContentTable.sql
+++ db/migrations/20141121100318_CreateContentTable.sql
@@ -1,7 +1,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-create table block
+create table blocks
 (
 	content_id int,
 	content text,
@@ -11,4 +11,4 @@ create table block
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
-drop table block;
+drop table blocks;
--- server.go
+++ server.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	//"github.com/gorilla/context"
+	"github.com/gorilla/context"
 	"github.com/codegangsta/negroni"
 	"net/http"
 	"github.com/gorilla/mux"
 	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"encoding/json"
 )
 
 func main(){
+
+	db, err := sqlx.Connect("postgres", "user=blogws dbname=blogws_dev password=blogws sslmode=disable")
+	Log(err)
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/blocks", Block_List)
 
 	n := negroni.Classic()
+
+	n.Use(negroni.HandlerFunc(
+		func (rw http.ResponseWriter, r *http.Request, next http.HandlerFunc){
+			context.Set(r, "db", db)
+			next(rw, r)
+		}))
+	n.Use(negroni.HandlerFunc(JSONEncoderHandler))
 	n.UseHandler(r)
 	n.Run(":3000")
 }
@@ -21,4 +36,30 @@ func main(){
 func HomeHandler(w http.ResponseWriter, req *http.Request){
 	
 	fmt.Fprintf(w, "Hello World")
+}
+
+func JSONEncoderHandler(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc){
+	enc := json.NewEncoder(rw)
+	context.Set(r, "enc", enc)
+	next(rw, r)
+}
+
+func Log(err error){
+	if err != nil {
+        log.Fatalln(err)
+    }
+}
+
+func GetDB(r *http.Request) *sqlx.DB {
+    if rv := context.Get(r, "db"); rv != nil {
+        return rv.(*sqlx.DB)
+    }
+    return nil
+}
+
+func GetENC(r *http.Request) *json.Encoder {
+    if rv := context.Get(r, "enc"); rv != nil {
+        return rv.(*json.Encoder)
+    }
+    return nil
 }
